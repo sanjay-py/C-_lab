a0b789edfd0d1695ce2d3facd2016a01d6edc674
sanjay-py <79186041+sanjay-py@users.noreply.github.com>
2023-03-01 17:43:52 +0530

    Add files via upload
    Note that the folder is named as term_7. But it is actually term_6, had a confusion.

---
+++ term_6/term 7/term 7/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace term_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fILEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cOURSEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sTUDENTToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rEPORTToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cOURSEWISEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.yEARWISEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fILEToolStripMenuItem,
+            this.rEPORTToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(782, 33);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fILEToolStripMenuItem
+            // 
+            this.fILEToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cOURSEToolStripMenuItem,
+            this.sTUDENTToolStripMenuItem});
+            this.fILEToolStripMenuItem.Name = "fILEToolStripMenuItem";
+            this.fILEToolStripMenuItem.Size = new System.Drawing.Size(55, 29);
+            this.fILEToolStripMenuItem.Text = "FILE";
+            // 
+            // cOURSEToolStripMenuItem
+            // 
+            this.cOURSEToolStripMenuItem.Name = "cOURSEToolStripMenuItem";
+            this.cOURSEToolStripMenuItem.Size = new System.Drawing.Size(159, 30);
+            this.cOURSEToolStripMenuItem.Text = "COURSE";
+            this.cOURSEToolStripMenuItem.Click += new System.EventHandler(this.cOURSEToolStripMenuItem_Click);
+            // 
+            // sTUDENTToolStripMenuItem
+            // 
+            this.sTUDENTToolStripMenuItem.Name = "sTUDENTToolStripMenuItem";
+            this.sTUDENTToolStripMenuItem.Size = new System.Drawing.Size(159, 30);
+            this.sTUDENTToolStripMenuItem.Text = "STUDENT";
+            this.sTUDENTToolStripMenuItem.Click += new System.EventHandler(this.sTUDENTToolStripMenuItem_Click);
+            // 
+            // rEPORTToolStripMenuItem
+            // 
+            this.rEPORTToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cOURSEWISEToolStripMenuItem,
+            this.yEARWISEToolStripMenuItem});
+            this.rEPORTToolStripMenuItem.Name = "rEPORTToolStripMenuItem";
+            this.rEPORTToolStripMenuItem.Size = new System.Drawing.Size(87, 29);
+            this.rEPORTToolStripMenuItem.Text = "REPORT";
+            // 
+            // cOURSEWISEToolStripMenuItem
+            // 
+            this.cOURSEWISEToolStripMenuItem.Name = "cOURSEWISEToolStripMenuItem";
+            this.cOURSEWISEToolStripMenuItem.Size = new System.Drawing.Size(197, 30);
+            this.cOURSEWISEToolStripMenuItem.Text = "COURSE WISE";
+            this.cOURSEWISEToolStripMenuItem.Click += new System.EventHandler(this.cOURSEWISEToolStripMenuItem_Click);
+            // 
+            // yEARWISEToolStripMenuItem
+            // 
+            this.yEARWISEToolStripMenuItem.Name = "yEARWISEToolStripMenuItem";
+            this.yEARWISEToolStripMenuItem.Size = new System.Drawing.Size(197, 30);
+            this.yEARWISEToolStripMenuItem.Text = "YEAR WISE";
+            this.yEARWISEToolStripMenuItem.Click += new System.EventHandler(this.yEARWISEToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 467);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fILEToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cOURSEToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sTUDENTToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rEPORTToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cOURSEWISEToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem yEARWISEToolStripMenuItem;
+    }
+}
+
---
+++ term_6/term 7/term 7/REPORT1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace term_7
+{
+    partial class REPORT1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.sTUDENTDataSet2 = new term_7.STUDENTDataSet2();
+            this.sTUDENTBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.sTUDENTTableAdapter = new term_7.STUDENTDataSet2TableAdapters.STUDENTTableAdapter();
+            this.uSNDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.sNAMEDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.aDDRESSDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.yEARADDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sTUDENTDataSet2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sTUDENTBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(277, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Course Number:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(524, 126);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(211, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(424, 202);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 42);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.uSNDataGridViewTextBoxColumn,
+            this.sNAMEDataGridViewTextBoxColumn,
+            this.aDDRESSDataGridViewTextBoxColumn,
+            this.cIDDataGridViewTextBoxColumn,
+            this.yEARADDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.sTUDENTBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(206, 295);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(668, 150);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // sTUDENTDataSet2
+            // 
+            this.sTUDENTDataSet2.DataSetName = "STUDENTDataSet2";
+            this.sTUDENTDataSet2.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // sTUDENTBindingSource
+            // 
+            this.sTUDENTBindingSource.DataMember = "STUDENT";
+            this.sTUDENTBindingSource.DataSource = this.sTUDENTDataSet2;
+            // 
+            // sTUDENTTableAdapter
+            // 
+            this.sTUDENTTableAdapter.ClearBeforeFill = true;
+            // 
+            // uSNDataGridViewTextBoxColumn
+            // 
+            this.uSNDataGridViewTextBoxColumn.DataPropertyName = "USN";
+            this.uSNDataGridViewTextBoxColumn.HeaderText = "USN";
+            this.uSNDataGridViewTextBoxColumn.Name = "uSNDataGridViewTextBoxColumn";
+            // 
+            // sNAMEDataGridViewTextBoxColumn
+            // 
+            this.sNAMEDataGridViewTextBoxColumn.DataPropertyName = "SNAME";
+            this.sNAMEDataGridViewTextBoxColumn.HeaderText = "SNAME";
+            this.sNAMEDataGridViewTextBoxColumn.Name = "sNAMEDataGridViewTextBoxColumn";
+            // 
+            // aDDRESSDataGridViewTextBoxColumn
+            // 
+            this.aDDRESSDataGridViewTextBoxColumn.DataPropertyName = "ADDRESS";
+            this.aDDRESSDataGridViewTextBoxColumn.HeaderText = "ADDRESS";
+            this.aDDRESSDataGridViewTextBoxColumn.Name = "aDDRESSDataGridViewTextBoxColumn";
+            // 
+            // cIDDataGridViewTextBoxColumn
+            // 
+            this.cIDDataGridViewTextBoxColumn.DataPropertyName = "CID";
+            this.cIDDataGridViewTextBoxColumn.HeaderText = "CID";
+            this.cIDDataGridViewTextBoxColumn.Name = "cIDDataGridViewTextBoxColumn";
+            // 
+            // yEARADDataGridViewTextBoxColumn
+            // 
+            this.yEARADDataGridViewTextBoxColumn.DataPropertyName = "YEARAD";
+            this.yEARADDataGridViewTextBoxColumn.HeaderText = "YEARAD";
+            this.yEARADDataGridViewTextBoxColumn.Name = "yEARADDataGridViewTextBoxColumn";
+            // 
+            // REPORT1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1091, 570);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "REPORT1";
+            this.Text = "REPORT1";
+            this.Load += new System.EventHandler(this.REPORT1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sTUDENTDataSet2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sTUDENTBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private STUDENTDataSet2 sTUDENTDataSet2;
+        private System.Windows.Forms.BindingSource sTUDENTBindingSource;
+        private STUDENTDataSet2TableAdapters.STUDENTTableAdapter sTUDENTTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn uSNDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn sNAMEDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn aDDRESSDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn yEARADDataGridViewTextBoxColumn;
+    }
+}
